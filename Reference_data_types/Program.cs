516d18005c504289dd7263845256ec32eacc54a8
Jonatandb <jonatandb@gmail.com>
2024-03-06 19:55:45 -0300

    Reference types and new keyword

---
+++ Reference_data_types/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+// Se puede usar new, pero también se puede omitir y
+// simplemente usar: string test = "test";
+string test = new string("test");
+System.Console.WriteLine(test);
+
+
+int[] numbers = new int[4];
+numbers[2] = 1230;
+
+foreach (int valor in numbers)
+{
+	Console.Write(valor + " ");
+}
