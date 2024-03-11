f2df13c808b82b2a18c1df2a5961a62850237b4e
Jonatandb <jonatandb@gmail.com>
2024-03-11 19:24:39 -0300

    Testing ToCharArray(), Array.Reverse(), string.Join() and string.Split()

---
+++ String_ToArrayChar_Join_Split/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+string name = "Jonatandb";
+
+char[] lettersArray = name.ToCharArray();
+
+Array.Reverse(lettersArray);
+
+string reversedName = new(lettersArray);
+Console.WriteLine(reversedName);
+
+string result = string.Join(",", lettersArray);
+Console.WriteLine(result);
+
+string[] letters = result.Split(",");
+foreach (string letter in letters) {
+	Console.WriteLine(letter);
+}
