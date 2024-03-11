9cb549f441392bfba8c4f913d5060247ddb3bf5b
Jonatandb <jonatandb@gmail.com>
2024-03-11 19:25:35 -0300

    Desafío para invertir las palabras de una oración

--- String_ToArrayChar_Join_Split/Program.cs
+++ String_ToArrayChar_Join_Split/Program.cs
@@ -1,18 +1,31 @@
 ﻿Console.Clear();
 
-string name = "Jonatandb";
+//string name = "Jonatandb";
 
-char[] lettersArray = name.ToCharArray();
+//char[] lettersArray = name.ToCharArray();
 
-Array.Reverse(lettersArray);
+//Array.Reverse(lettersArray);
 
-string reversedName = new(lettersArray);
-Console.WriteLine(reversedName);
+//string reversedName = new(lettersArray);
+//Console.WriteLine(reversedName);
 
-string result = string.Join(",", lettersArray);
-Console.WriteLine(result);
+//string result = string.Join(",", lettersArray);
+//Console.WriteLine(result);
 
-string[] letters = result.Split(",");
-foreach (string letter in letters) {
-	Console.WriteLine(letter);
-}
+//string[] letters = result.Split(",");
+//foreach (string letter in letters) {
+//	Console.WriteLine(letter);
+//}
+
+
+// Desafío para invertir las palabras de una oración:
+string pangram = "The quick brown fox jumps over the lazy dog";
+string finalPhrase = "";
+string[] words = pangram.Split(" ");
+foreach (string word in words) {
+	char[] letters = word.ToCharArray();
+	Array.Reverse(letters);
+	string reversedWord = string.Join("", letters);
+	finalPhrase += reversedWord + " ";
+} 
+Console.WriteLine(finalPhrase);
