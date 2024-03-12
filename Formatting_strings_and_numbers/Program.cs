06d30c478da63029c2d153c51536ca21207acc3f
Jonatandb <jonatandb@gmail.com>
2024-03-12 19:10:48 -0300

    Formatting percentage numbers

--- Formatting_strings_and_numbers/Program.cs
+++ Formatting_strings_and_numbers/Program.cs
@@ -1,4 +1,6 @@
-﻿// string.Format
+﻿// https://learn.microsoft.com/es-es/training/modules/csharp-format-strings/2-string-formatting-basics
+
+// string.Format
 //string _first = "Hello";
 //string _second = "World";
 //string result = string.Format("{0} {1}!", _first, _second);
@@ -17,12 +19,17 @@
 //decimal price = 123.45m;
 //int discount = 50;
 //Console.WriteLine($"Price: {price:C} (Save {discount:C})");
-//// Price: $ 123,45 (Save $ 50,00)
+////	Price: $ 123,45 (Save $ 50,00)
+
+// Formatting decimal numbers
+//decimal measurement = 123456.78912m;
+//Console.WriteLine($"Measurement: {measurement:N} units");
+////	Measurement: 123.456,79 units
+//Console.WriteLine($"Measurement: {measurement:N4} units");
+////	Measurement: 123.456,7891 units
 
 
-decimal measurement = 123456.78912m;
-Console.WriteLine($"Measurement: {measurement:N} units");
-// Measurement: 123.456,79 units
-Console.WriteLine($"Measurement: {measurement:N4} units");
-// Measurement: 123.456,7891 units
-
+// Formatting percentage numbers
+decimal tax = 0.36785m;
+Console.WriteLine($"Tax rate: {tax:P2}");
+//	Tax rate: 36,79 %
