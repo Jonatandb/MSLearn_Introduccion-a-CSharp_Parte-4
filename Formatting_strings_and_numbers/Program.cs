0c9a67fbff02b08cef8f4b5d0590d6e35cadb550
Jonatandb <jonatandb@gmail.com>
2024-03-12 19:00:23 -0300

    Using $ for string interpolation

--- Formatting_strings_and_numbers/Program.cs
+++ Formatting_strings_and_numbers/Program.cs
@@ -1,5 +1,13 @@
 ﻿// string.Format
-string _first = "Hello";
-string _second = "World";
-string result = string.Format("{0} {1}!", _first, _second);
-Console.WriteLine(result);
+//string _first = "Hello";
+//string _second = "World";
+//string result = string.Format("{0} {1}!", _first, _second);
+//Console.WriteLine(result);
+
+
+// Using $ for string interpolation
+string first = "Hello";
+string second = "World";
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
